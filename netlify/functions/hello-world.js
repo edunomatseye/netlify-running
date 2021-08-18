a3b9f180ce6446ba9f99ff8459fe867365d32aa7
Edun Omatseye <edunomatseye@yahoo.com>
2021-08-18 23:38:19 +0100

    Object notation resolved

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,6 +2,6 @@ exports.handler = async function(event, context) {
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "hello world!!" })
+        body: JSON.stringify({ message: "hello world!!!!" })
     }
 }
